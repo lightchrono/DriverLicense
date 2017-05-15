ef38d0fd1294d1c00e92efc95682594eb1a9b132
Tavi <goreaoctavian@live.com>
2017-05-15 11:43:32 +0300

    Even more namefixes and values fixes

--- app/src/main/java/com/example/intern01/driverlicense/other/CircleTransform.java
+++ app/src/main/java/com/example/intern01/driverlicense/other/CircleTransform.java
@@ -10,9 +10,10 @@ import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
-import com.bumptech
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
 
-public class CircleTransform extends Bitmap {
+public class CircleTransform extends BitmapTransformation {
     public CircleTransform(Context context) {
         super(context);
     }
