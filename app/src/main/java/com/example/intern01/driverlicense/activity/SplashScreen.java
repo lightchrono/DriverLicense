b1e66a67527ec7952fb4b79aa9ec96aeebf99264
Light <lightchrono@gmail.com>
2017-05-19 15:42:58 +0300

    do some fixes or add some code or something

--- app/src/main/java/com/example/intern01/driverlicense/activity/SplashScreen.java
+++ app/src/main/java/com/example/intern01/driverlicense/activity/SplashScreen.java
@@ -20,6 +20,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
         context = this;
 
                 SharedPreferences pref = getSharedPreferences("loginC",MODE_PRIVATE);
