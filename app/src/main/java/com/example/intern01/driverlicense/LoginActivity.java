e6f7b73111c7d5750c290b5f57d0112dd806f272
Gorea Octavian <goreaoctavian@live.com>
2017-05-22 14:37:48 +0300

    Cleanup

--- app/src/main/java/com/example/intern01/driverlicense/LoginActivity.java
+++ app/src/main/java/com/example/intern01/driverlicense/LoginActivity.java
@@ -20,21 +20,10 @@ import android.widget.Toast;
 import com.example.intern01.driverlicense.activity.MainActivity;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.common.SignInButton;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
